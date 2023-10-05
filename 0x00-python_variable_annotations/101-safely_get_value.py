1348da5b7c1737a39a6f86e1734acf2150c55716
abel-udoh <abeludoh8@gmail.com>
2023-10-05 08:06:33 -0700

    python variable annotation

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ More involved type annotations """
+
+
 from typing import Any, Union, Mapping, TypeVar
+
+
 T = TypeVar('T')
-
-
-def safely_get_value(dct: Mapping, key: Any,
--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -3,3 +3,5 @@
 returns a function that multiplies a float by multiplier """
 
 from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
